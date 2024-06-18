718416189605d96fe172dbea6d92ee2211856c66
Richert <richgast.90@gmail.com>
2024-06-18 14:11:23 -0500

    updates to the lorenz prediction scripts

--- scripts/plotting_lorenz.py
+++ scripts/plotting_lorenz.py
@@ -63,7 +63,7 @@ noise = [0.1, 0.5, 0.9]
 trajectories = {model: [] for model in models}
 for lvl in noise:
 
-    df_tmp = df.loc[np.abs(df.loc[:, "noise"] - lvl) < eps, :]
+    df_tmp = df.loc[np.round(df.loc[:, "noise"], decimals=1) == lvl, :]
 
     for model in models:
 
