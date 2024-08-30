870ebe6a2ecb0b916249106cbaef057e1ff22bfb
Richert <richgast.90@gmail.com>
2024-08-30 13:43:08 -0700

    new scripts for learning piano songs

--- scripts/piano_crnn_cluster.py
+++ scripts/piano_crnn_cluster.py
@@ -27,6 +27,7 @@ dtype = torch.float64
 device = "cpu"
 plot_steps = 200
 state_vars = ["x", "y", "z"]
+n_reps = 10
 
 # piano pattern parameters
 keys = int(sys.argv[-3])
@@ -57,88 +58,100 @@ loading_trials = 100000
 alphas = (alpha, 1e-4)
 init_steps = 1000
 
-# generate inputs and targets
-#############################
-
-# generate all possible patterns
+# get input patterns
 input_patterns, target_patterns = data["inputs"], data["targets"]
 n_patterns = len(input_patterns)
 
-# generate random motifs
-motif_col, target_col = [], []
-for _ in range(motifs):
-    X, y = get_trials(input_patterns, target_patterns, motif_length, key_dur)
-    motif_col.append(np.asarray(X).reshape((motif_length*key_dur, n_in), order="C"))
-    target_col.append(np.asarray(y).reshape((motif_length*key_dur, n_out), order="C"))
-
-# train RNN to generate correct key press patterns
-##################################################
-
-# initialize LR-RNN
-rnn = ConceptorRNN(W, W_in, bias)
-
-# initial wash-out period
-avg_input = torch.zeros(size=(n_in,), device=device, dtype=dtype)
-with torch.no_grad():
-    for step in range(init_steps):
-        rnn.forward(avg_input)
-
-# train conceptor for each motif
-for i, (motif, target) in enumerate(zip(motif_col, target_col)):
-
-    motif = torch.tensor(motif, device=device, dtype=dtype)
-
-    # collect states
-    state_col = []
-    with torch.no_grad():
-        for trial in range(training_trials):
-            for step in range(motif.shape[0]):
-                state_col.append(rnn.forward(motif[step]+noise_lvl*torch.randn((n_in,), device=device, dtype=dtype)))
-
-    # train conceptor
-    rnn.learn_conceptor(f"motif_{i}", torch.stack(state_col, dim=0), alpha=alphas[0])
-
-# input loading
-state_col, input_col = [], []
-for i, (motif, target) in enumerate(zip(motif_col, target_col)):
-    with torch.no_grad():
-        motif = torch.tensor(motif, device=device, dtype=dtype)
-        rnn.activate_conceptor(f"motif_{i}")
-        for trial in range(loading_trials):
-            for step in range(motif.shape[0]):
-                state_col.append(rnn.y)
-                input_col.append(motif[step])
-                rnn.forward_c(motif[step]+noise_lvl*torch.randn((n_in,), device=device, dtype=dtype))
-_, epsilon = rnn.load_input(torch.stack(state_col, dim=0).T, torch.stack(input_col, dim=0).T, tychinov_alpha=alphas[1])
-print(f"Input loading error: {epsilon}")
-
-# test conceptor-controlled RNN performance
-###########################################
-
-results = {"alpha": alpha, "motifs": motifs, "motif_length": motif_length,
-           "epsilon": epsilon, "conceptors": [], "sequence_predictions": [], "sequence_targets": [],
+# results collection
+results = {"alpha": alpha, "motifs": motifs, "motif_length": motif_length, "n_reps": n_reps,
+           "epsilon": [], "conceptors": [], "sequence_predictions": [], "sequence_targets": [],
            "input_predictions": [], "input_targets": []}
-for i, (motif, target) in enumerate(zip(motif_col, target_col)):
+for n in range(n_reps):
 
-    rnn.activate_conceptor(f"motif_{i}")
-    C = rnn.C.cpu().detach().numpy()
-    results["conceptors"].append(C)
+    # generate inputs and targets
+    #############################
 
-    # collect predictions for input-driven dynamics
+    # generate random motifs
+    motif_col, target_col = [], []
+    for _ in range(motifs):
+        X, y = get_trials(input_patterns, target_patterns, motif_length, key_dur)
+        motif_col.append(np.asarray(X).reshape((motif_length*key_dur, n_in), order="C"))
+        target_col.append(np.asarray(y).reshape((motif_length*key_dur, n_out), order="C"))
+
+    # train RNN to generate correct key press patterns
+    ##################################################
+
+    # initialize LR-RNN
+    rnn = ConceptorRNN(W, W_in, bias)
+
+    # initial wash-out period
+    avg_input = torch.zeros(size=(n_in,), device=device, dtype=dtype)
     with torch.no_grad():
-        for trial in range(test_trials):
-            for step in range(motif.shape[0]):
-                y = W_r @ rnn.forward_c(motif[step])
-                results["input_predictions"].append(y)
-                results["input_targets"].append(target[step])
+        for step in range(init_steps):
+            rnn.forward(avg_input)
 
-    # collect predictions for sequence generation
-    with torch.no_grad():
-        for trial in range(test_trials):
-            for step in range(motif.shape[0]):
-                y = W_r @ rnn.forward_c_a()
-                results["sequence_predictions"].append(y)
-                results["sequence_targets"].append(target[step])
+    # train conceptor for each motif
+    for i, (motif, target) in enumerate(zip(motif_col, target_col)):
+
+        motif = torch.tensor(motif, device=device, dtype=dtype)
+
+        # collect states
+        state_col = []
+        with torch.no_grad():
+            for trial in range(training_trials):
+                for step in range(motif.shape[0]):
+                    state_col.append(rnn.forward(motif[step]+noise_lvl*torch.randn((n_in,), device=device, dtype=dtype)))
+
+        # train conceptor
+        rnn.learn_conceptor(f"motif_{i}", torch.stack(state_col, dim=0), alpha=alphas[0])
+
+    # input loading
+    state_col, input_col = [], []
+    for i, (motif, target) in enumerate(zip(motif_col, target_col)):
+        with torch.no_grad():
+            motif = torch.tensor(motif, device=device, dtype=dtype)
+            rnn.activate_conceptor(f"motif_{i}")
+            for trial in range(loading_trials):
+                for step in range(motif.shape[0]):
+                    state_col.append(rnn.y)
+                    input_col.append(motif[step])
+                    rnn.forward_c(motif[step]+noise_lvl*torch.randn((n_in,), device=device, dtype=dtype))
+    _, epsilon = rnn.load_input(torch.stack(state_col, dim=0).T, torch.stack(input_col, dim=0).T, tychinov_alpha=alphas[1])
+    results["epsilon"].append(epsilon)
+
+    # test conceptor-controlled RNN performance
+    ###########################################
+
+    conceptors, inp_predictions, inp_targets, seq_predictions, seq_targets = [], [], [], [], []
+    for i, (motif, target) in enumerate(zip(motif_col, target_col)):
+
+        rnn.activate_conceptor(f"motif_{i}")
+        C = rnn.C.cpu().detach().numpy()
+        conceptors.append(C)
+
+        # collect predictions for input-driven dynamics
+        with torch.no_grad():
+            for trial in range(test_trials):
+                for step in range(motif.shape[0]):
+                    y = W_r @ rnn.forward_c(motif[step])
+                    inp_predictions.append(y)
+                    inp_targets.append(target[step])
+
+        # collect predictions for sequence generation
+        with torch.no_grad():
+            for trial in range(test_trials):
+                for step in range(motif.shape[0]):
+                    y = W_r @ rnn.forward_c_a()
+                    seq_predictions.append(y)
+                    seq_targets.append(target[step])
+
+    # store results
+    results["conceptors"].append(conceptors)
+    results["input_predictions"].append(inp_predictions)
+    results["input_targets"].append(inp_targets)
+    results["sequence_predictions"].append(seq_predictions)
+    results["sequence_targets"].append(seq_targets)
+    print(f"Finished {n+1} of {n_reps} random trials.")
 
 # save results
 pickle.dump(results, open(f"{path}/data/piano/piano_crnn_{keys}keys_{fingers}fingers_{cond}.pkl", "wb"))
--- scripts/piano_crnn_plotting.py
+++ scripts/piano_crnn_plotting.py
@@ -4,6 +4,7 @@ from pandas import DataFrame
 import pickle
 import sys
 import os
+import seaborn as sb
 
 # condition
 path = sys.argv[-3]
@@ -26,19 +27,50 @@ for f in os.listdir(path):
         results["motif_length"].append(data["motif_length"])
 
         # calculate error between predictions and targets for input-driven mode
-        predictions = data["input_predictions"]
-        targets = data["input_targets"]
+        predictions = np.asarray(data["input_predictions"]).flatten()
+        targets = np.asarray(data["input_targets"]).flatten()
         results["input_error"].append(np.mean((predictions - targets)**2))
 
         # calculate error between predictions and targets for autonomous mode
-        predictions = data["sequence_predictions"]
-        targets = data["sequence_targets"]
+        predictions = np.asarray(data["sequence_predictions"]).flatten()
+        targets = np.asarray(data["sequence_targets"]).flatten()
         results["sequence_error"].append(np.mean((predictions - targets) ** 2))
 
 df = DataFrame.from_dict(results)
-n_motifs = len(np.unique(df["motifs"].values))
-n_motif_lengths = len(np.unique(df["motif_length"].values))
+motifs = np.unique(df["motifs"].values)
+motif_lengths = np.unique(df["motif_length"].values)
+n_motifs = len(motifs)
+n_motif_lengths = len(motif_lengths)
 
-# plot results
-fig = plt.figure(figsize=(12, 9))
-grid = fig.add_gridspec(nrows=n_motifs, ncols=n_motif_lengths)
+# plot input-driven results
+fig = plt.figure(figsize=(12, 3))
+grid = fig.add_gridspec(nrows=1, ncols=n_motif_lengths)
+for i in range(n_motif_lengths):
+    l = motif_lengths[i]
+    df_tmp = df.loc[df["motif_length"] == l, :]
+    ax = fig.add_subplot(grid[0, i])
+    sb.lineplot(data=df_tmp, x="alpha", y="input_error", hue="motifs")
+    ax.set_xlabel("alpha")
+    ax.set_xscale("log")
+    if i == 0:
+        ax.set_ylabel("MSE")
+fig.suptitle("Input-driven motor command mode")
+plt.tight_layout()
+
+# plot sequence generation results
+fig = plt.figure(figsize=(12, 3))
+grid = fig.add_gridspec(nrows=1, ncols=n_motif_lengths)
+for i in range(n_motif_lengths):
+    l = motif_lengths[i]
+    df_tmp = df.loc[df["motif_length"] == l, :]
+    ax = fig.add_subplot(grid[0, i])
+    sb.lineplot(data=df_tmp, x="alpha", y="sequence_error", hue="motifs")
+    ax.set_xlabel("alpha")
+    ax.set_xscale("log")
+    ax.set_title(f"Motif length: {i+1}")
+    if i == 0:
+        ax.set_ylabel("MSE")
+fig.suptitle("Autonomous motor sequence generation mode")
+plt.tight_layout()
+
+plt.show()
