2c41413b59cb9b9caa858f40cb2cde2f72a17154
Richert <richgast.90@gmail.com>
2025-04-12 12:56:59 -0700

    new scripts for learning piano songs

--- scripts/clr_rhythmic_fit.py
+++ scripts/clr_rhythmic_fit.py
@@ -11,12 +11,13 @@ import pickle
 # general
 dtype = torch.float64
 device = "cpu"
-plot_examples = 6
 state_vars = ["y"]
-visualization = {"connectivity": True, "inputs": True, "results": True}
+path = "/home/richard"
+load_file = f"{path}/data/vdp_3freqs.pkl"
+save_file = f"{path}/results/clr_rhythmic_fits.pkl"
 
 # load inputs and targets
-data = pickle.load(open("../data/vdp_2freqs.pkl", "rb"))
+data = pickle.load(open(load_file, "rb"))
 inputs = data["inputs"]
 targets = data["targets"]
 conditions = data["trial_conditions"]
@@ -55,247 +56,129 @@ alpha = 1e-1
 batches = int(augmentation * train_trials / batch_size)
 
 # sweep parameters
-Delta = [0.05, 0.2]
-sigma = []
-n_reps = 20
-n_trials = len(lam)*len(Delta)*len(sigma)*n_reps
+Delta = [0.1, 0.4]
+sigma = np.arange(start=0.4, stop=2.5, step=0.2)
+n_reps = 10
+n_trials = len(Delta)*len(sigma)*n_reps
 
 # prepare results
-results = {"lambda": [], "Delta": [], "sigma": [], "trial": [], "z_memory": [],
-           "z_perturbed": [], "z_unperturbed": [], "x": []}
+results = {"Delta": [], "sigma": [], "trial": [], "train_epochs": [], "train_loss": [], "test_loss": []}
 
-# initialize rnn matrices
-bias = torch.tensor(Delta * np.random.randn(k), device=device, dtype=dtype)
-W_in = torch.tensor(in_scale * np.random.randn(N, n_in), device=device, dtype=dtype)
-L = init_weights(N, k, density) * sigma
-W, R = init_dendrites(k, n_dendrites)
-W_r = torch.tensor(out_scale * np.random.randn(n_out, k), device=device, dtype=dtype)
+# model training
+################
 
-# plot connectivity
-if visualization["connectivity"]:
-    fig_conn, axes_conn = plt.subplots(ncols=3, nrows=2, figsize=(12, 5))
-    ax = axes_conn[0, 0]
-    ax.imshow(L, aspect="auto", interpolation="none")
-    ax.set_xlabel("from: neurons")
-    ax.set_ylabel("to: dendrites")
-    ax.set_title("Synaptic weights (should change)")
-    ax = axes_conn[0, 1]
-    ax.imshow(R, aspect="auto", interpolation="none")
-    ax.set_title("Dendrite-soma interactions (shouldn't change)")
-    ax.set_xlabel("from: dendrites")
-    ax.set_ylabel("to: neurons")
-    ax = axes_conn[0, 2]
-    ax.imshow(W, aspect="auto", interpolation="none")
-    ax.set_title("Dendritic interactions (shouldn't change)")
-    ax.set_xlabel("from: dendrites")
-    ax.set_ylabel("to: dendrites")
-    plt.tight_layout()
-
-# plot inputs and targets
-if visualization["inputs"]:
-    fig, axes = plt.subplots(nrows=plot_examples, figsize=(12, 2 * plot_examples))
-    for i, trial in enumerate(np.random.choice(train_trials, size=(plot_examples,))):
-        ax = axes[i]
-        ax.plot(inputs[trial], label="x")
-        ax.plot(targets[trial], label=state_vars[0])
-        ax.set_xlabel("time")
-        ax.set_ylabel("amplitude")
-        ax.set_title(f"training trial {trial + 1}: in-phase = {conditions[trial]}")
-        ax.legend()
-    fig.suptitle("Inputs (x) and Target Waveforms (y)")
-    plt.tight_layout()
-
-# train LR-RNN weights
-######################
-
-# model initialization
-rnn = LowRankCRNN(torch.tensor(W*lam, dtype=dtype, device=device),
-                  torch.tensor(L*(1-lam), dtype=dtype, device=device),
-                  torch.tensor(R, device=device, dtype=dtype), W_in, bias, g="ReLU", lam=1e-3, alpha=10.0)
-rnn.free_param("W_in")
-rnn.free_param("bias")
-rnn.free_param("L")
-
-# input definition
-inp = torch.zeros((steps, n_in), device=device, dtype=dtype)
-
-# get initial state
-with torch.no_grad():
-    for step in range(init_steps):
-        x = torch.randn(n_in, dtype=dtype, device=device)
-        rnn.forward(x)
-init_state = [v.detach() + epsilon*torch.randn(v.shape[0]) for v in rnn.state_vars]
-
-# initialize z controllers
-unique_conditions = np.unique(conditions)
-# for c in unique_conditions:
-#     rnn.init_new_z_controller(init_value="ones")
-#     rnn.store_z_controller(c)
-
-# set up loss function
-loss_func = torch.nn.MSELoss()
-
-# set up optimizer
-optim = torch.optim.Adam(list(rnn.parameters()) + [W_r], lr=lr, betas=betas)
-rnn.clip(gradient_cutoff)
-
-# training
-current_loss = 0.0
-loss_col = []
 with torch.enable_grad():
-    for batch in range(batches):
+    n = 0
+    for Delta_tmp in Delta:
+        for sigma_tmp in sigma:
+            for rep in range(n_reps):
 
-        loss = torch.zeros((1,))
+                # initialize rnn matrices
+                bias = torch.tensor(Delta_tmp * np.random.randn(k), device=device, dtype=dtype)
+                W_in = torch.tensor(in_scale * np.random.randn(N, n_in), device=device, dtype=dtype)
+                L = init_weights(N, k, density)
+                W, R = init_dendrites(k, n_dendrites)
+                W_r = torch.tensor(out_scale * np.random.randn(n_out, k), device=device, dtype=dtype)
 
-        for trial in np.random.choice(train_trials, size=(batch_size,), replace=False):
+                # model initialization
+                rnn = LowRankCRNN(torch.tensor(W*lam, dtype=dtype, device=device),
+                                  torch.tensor(L*(1-lam), dtype=dtype, device=device),
+                                  torch.tensor(R, device=device, dtype=dtype), W_in, bias, g="ReLU")
+                rnn.C_z *= sigma_tmp
+                rnn.free_param("W_in")
+                rnn.free_param("bias")
+                rnn.free_param("L")
 
-            # get input and target timeseries
-            inp = torch.tensor(inputs[trial], device=device, dtype=dtype)
-            target = torch.tensor(targets[trial], device=device, dtype=dtype)
+                # get initial state
+                with torch.no_grad():
+                    for step in range(init_steps):
+                        x = torch.randn(n_in, dtype=dtype, device=device)
+                        rnn.forward(x)
+                init_state = [v.detach() + epsilon*torch.randn(v.shape[0]) for v in rnn.state_vars]
 
-            # initial condition
-            rnn.detach()
-            rnn.set_state(init_state)
-            # rnn.activate_z_controller(conditions[trial])
+                # set up loss function
+                loss_func = torch.nn.MSELoss()
 
-            # collect loss
-            y_col = []
-            for step in range(steps):
-                z = rnn.forward(inp[step])
-                rnn.update_z_controller()
-                y = W_r @ z
-                if step % truncation_steps == truncation_steps - 1:
-                    rnn.detach()
-                y_col.append(y)
+                # set up optimizer
+                optim = torch.optim.Adam(list(rnn.parameters()) + [W_r], lr=lr, betas=betas)
+                rnn.clip(gradient_cutoff)
 
-            # calculate loss
-            y_col = torch.stack(y_col, dim=0)
-            loss += loss_func(y_col, target)
-            # rnn.store_z_controller(conditions[trial])
+                # training
+                train_loss = 0.0
+                loss_col = []
+                with torch.enable_grad():
+                    for batch in range(batches):
 
-        # make update
-        # for c in rnn.z_controllers.values():
-        #     loss += alpha*torch.sum(c)
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
+                        loss = torch.zeros((1,))
 
-        # store and print loss
-        current_loss = loss.item()
-        loss_col.append(current_loss)
-        print(f"Training batch {batch + 1} / {batches}: MSE = {current_loss}")
-        if current_loss < epsilon:
-            break
+                        for trial in np.random.choice(train_trials, size=(batch_size,), replace=False):
 
-# generate predictions
-######################
+                            # get input and target timeseries
+                            inp = torch.tensor(inputs[trial], device=device, dtype=dtype)
+                            target = torch.tensor(targets[trial], device=device, dtype=dtype)
 
-predictions = []
-z_dynamics = []
-test_loss = []
-with torch.no_grad():
-    for trial in range(train_trials, trials):
+                            # initial condition
+                            rnn.detach()
+                            rnn.set_state(init_state)
 
-        # get input and target timeseries
-        inp = torch.tensor(inputs[trial], device=device, dtype=dtype)
-        target = torch.tensor(targets[trial], device=device, dtype=dtype)
+                            # collect loss
+                            y_col = []
+                            for step in range(steps):
+                                z = rnn.forward(inp[step])
+                                y = W_r @ z
+                                if step % truncation_steps == truncation_steps - 1:
+                                    rnn.detach()
+                                y_col.append(y)
 
-        # initial condition
-        rnn.set_state(init_state)
-        # rnn.activate_z_controller(conditions[trial])
+                            # calculate loss
+                            y_col = torch.stack(y_col, dim=0)
+                            loss += loss_func(y_col, target)
 
-        # make prediction
-        y_col, z_col = [], []
-        for step in range(steps):
-            z = rnn.forward(inp[step])
-            y = W_r @ z
-            y_col.append(y)
-            z_col.append(z)
+                        # make update
+                        optim.zero_grad()
+                        loss.backward()
+                        optim.step()
 
-        # calculate loss
-        loss = loss_func(torch.stack(y_col, dim=0), target)
+                        # store and print loss
+                        train_loss = loss.item()
+                        loss_col.append(train_loss)
+                        if train_loss < epsilon:
+                            break
 
-        # save predictions
-        predictions.append(np.asarray(y_col))
-        z_dynamics.append(np.asarray(z_col))
-        test_loss.append(loss.item())
+                # generate predictions
+                test_loss = []
+                with torch.no_grad():
+                    for trial in range(train_trials, trials):
+
+                        # get input and target timeseries
+                        inp = torch.tensor(inputs[trial], device=device, dtype=dtype)
+                        target = torch.tensor(targets[trial], device=device, dtype=dtype)
+
+                        # initial condition
+                        rnn.set_state(init_state)
+
+                        # make prediction
+                        y_col = []
+                        for step in range(steps):
+                            z = rnn.forward(inp[step])
+                            y = W_r @ z
+                            y_col.append(y)
+
+                        # calculate loss
+                        loss = loss_func(torch.stack(y_col, dim=0), target)
+                        test_loss.append(loss.item())
+
+                    # save results
+                    results["Delta"].append(Delta_tmp)
+                    results["sigma"].append(sigma_tmp)
+                    results["trial"].append(rep)
+                    results["train_epochs"].append(batch)
+                    results["train_loss"].append(loss_col)
+                    results["test_loss"].append(np.sum(test_loss))
+
+                    # report progress
+                    n += 1
+                    print(f"Finished {n} out of {n_trials} trials.")
 
 # save results
-results = {"predictions": predictions, "z_dynamics": z_dynamics, "train_loss": loss_col, "test_loss": test_loss,
-           "W_r": W_r, "y_controllers": {key: c.detach().cpu().numpy() for key, c in rnn.y_controllers.items()},
-           }
-results.update({key: getattr(rnn, key).detach().cpu().numpy() for key in ["L", "R", "W", "W_in", "bias"]})
-pickle.dump(results, open(f"../data/clr_rhythmic_fit.pkl", "wb"))
-
-# plotting
-##########
-
-if visualization["results"]:
-
-    # prediction figure
-    fig, axes = plt.subplots(nrows=plot_examples, figsize=(12, 2 * plot_examples))
-    for i, trial in enumerate(np.random.choice(test_trials, size=(plot_examples,))):
-        ax = axes[i]
-        ax.plot(targets[train_trials + trial], label="target", linestyle="dashed", color="black")
-        ax.plot(predictions[trial], label="prediction", linestyle="solid", color="darkorange")
-        ax.axvline(x=int(0.5 * steps), color="grey", linestyle="dashed")
-        ax.set_ylabel("amplitude")
-        ax.set_title(f"test trial {trial + 1}")
-        if i == plot_examples - 1:
-            ax.set_xlabel("steps")
-            ax.legend()
-    fig.suptitle("Model Predictions")
-    plt.tight_layout()
-
-    # dynamics figure
-    fig, axes = plt.subplots(nrows=plot_examples, figsize=(12, 2 * plot_examples))
-    n_neurons = 5
-    for i in range(plot_examples):
-        mean_v = np.mean(z_dynamics[i], axis=-1)
-        ax = axes[i]
-        ax.plot(mean_v, color="black", label="mean")
-        for j in np.random.choice(k, size=(n_neurons,)):
-            ax.plot(z_dynamics[i][:, j], label=f"neuron {j + 1}")
-        ax.axvline(x=int(0.5 * steps), color="grey", linestyle="dashed")
-        ax.set_ylabel("amplitude")
-        if i == plot_examples - 1:
-            ax.set_xlabel("steps")
-            ax.legend()
-    fig.suptitle("RNN dynamics")
-    plt.tight_layout()
-
-    # conceptors figure
-    # conceptors = np.asarray([c.detach().cpu().numpy() for c in rnn.z_controllers.values()])
-    # fig, ax = plt.subplots(figsize=(12, 2*len(conceptors)))
-    # im = ax.imshow(conceptors, aspect="auto", interpolation="none", cmap="cividis")
-    # plt.colorbar(im, ax=ax, shrink=0.8)
-    # ax.set_xlabel("neurons")
-    # ax.set_ylabel("conditions")
-    # ax.set_title("Conceptors")
-    # plt.tight_layout()
-
-    # training loss figure
-    fig, ax = plt.subplots(figsize=(12, 4))
-    ax.plot(loss_col)
-    ax.set_xlabel("training batch")
-    ax.set_ylabel("MSE")
-    ax.set_title("Training loss")
-    plt.tight_layout()
-
-# plot connectivity
-if visualization["connectivity"]:
-    ax = axes_conn[1, 0]
-    ax.imshow(rnn.L.detach().cpu().numpy(), aspect="auto", interpolation="none")
-    ax.set_xlabel("from: neurons")
-    ax.set_ylabel("to: dendrites")
-    ax = axes_conn[1, 1]
-    ax.imshow(rnn.R.detach().cpu().numpy(), aspect="auto", interpolation="none")
-    ax.set_xlabel("from: dendrites")
-    ax.set_ylabel("to: neurons")
-    ax = axes_conn[1, 2]
-    ax.imshow(rnn.W.detach().cpu().numpy(), aspect="auto", interpolation="none")
-    ax.set_xlabel("from: dendrites")
-    ax.set_ylabel("to: dendrites")
-    plt.tight_layout()
-
-    plt.show()
+pickle.dump(results, open(save_file, "wb"))
