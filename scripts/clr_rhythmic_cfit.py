d8722b654fc879c70dc07f90403f263f04ba0d8e
Richert <richgast.90@gmail.com>
2025-05-02 09:52:26 -0700

    updates on rhythmic learning task

--- scripts/clr_rhythmic_cfit.py
+++ scripts/clr_rhythmic_cfit.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 # general
 n_conditions = 3
 dtype = torch.float64
-device = "cuda:0"
+device = "cuda:1"
 state_vars = ["y"]
 path = "/home/richard"
 load_file = f"{path}/data/vdp_{n_conditions}freqs.pkl"
