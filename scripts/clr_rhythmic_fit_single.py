ff313710c1e09908ca33c7534b7bcbeedf82e96d
Richert <richgast.90@gmail.com>
2025-05-04 13:18:26 -0700

    updates on rhythmic learning task

---
+++ scripts/clr_rhythmic_fit_single.py
@@ -0,0 +1,225 @@
+import sys
+sys.path.append("../")
+from src.rnn import LowRankCRNN
+from src.functions import init_weights, init_dendrites
+import torch
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+# parameter definition
+######################
+
+# general
+n_conditions = 3
+dtype = torch.float64
+device = "cuda:0"
+state_vars = ["y"]
+path = "/home/richard-gast/Documents"
+load_file = f"{path}/data/vdp_{n_conditions}freqs.pkl"
+save_file = f"{path}/results/clr_rhythmic_{n_conditions}freqs_fit_single.pkl"
+visualize_results = True
+plot_examples = 5
+
+# load inputs and targets
+data = pickle.load(open(load_file, "rb"))
+inputs = data["inputs"]
+targets = data["targets"]
+conditions = data["trial_conditions"]
+unique_conditions = np.unique(conditions, axis=0)
+
+# add condition information to input
+inputs_new = []
+for inp, cond in zip(inputs, conditions):
+    inp_new = np.zeros((inp.shape[0], inp.shape[1]+n_conditions))
+    idx = np.argwhere([tuple(uc) == cond for uc in unique_conditions]).squeeze()
+    inp_new[:, :-n_conditions] = inp
+    inp_new[:, idx-n_conditions] = 1.0
+    inputs_new.append(inp_new.copy())
+inputs = inputs_new
+
+# task parameters
+steps = inputs[0].shape[0]
+init_steps = 20
+auto_steps = 100
+noise_lvl = 0.0
+
+# rnn parameters
+k = 100
+n_dendrites = 10
+n_in = inputs[0].shape[-1]
+n_out = targets[0].shape[-1]
+density = 0.5
+in_scale = 0.1
+out_scale = 0.2
+Delta = 0.1
+sigma = 0.6
+N = int(k * n_dendrites)
+
+# training parameters
+trials = len(conditions)
+train_trials = int(0.9 * trials)
+test_trials = trials - train_trials
+augmentation = 1.0
+lr = 1e-2
+betas = (0.9, 0.999)
+batch_size = 20
+gradient_cutoff = 1e10
+truncation_steps = 100
+epsilon = 0.3
+batches = int(augmentation * train_trials / batch_size)
+
+# model training
+################
+
+# initialize rnn matrices
+bias = torch.tensor(Delta * np.random.randn(k), device=device, dtype=dtype)
+W_in = torch.tensor(in_scale * np.random.randn(N, n_in), device=device, dtype=dtype)
+L = init_weights(N, k, density)
+W, R = init_dendrites(k, n_dendrites)
+W_r = torch.tensor(out_scale * np.random.randn(n_out, k), device=device, dtype=dtype, requires_grad=True)
+
+# model initialization
+rnn = LowRankCRNN(torch.tensor(W*0.0, dtype=dtype, device=device),
+                  torch.tensor(L*sigma, dtype=dtype, device=device),
+                  torch.tensor(R, device=device, dtype=dtype),
+                  W_in, bias, g="ReLU")
+rnn.free_param("L")
+
+# set up loss function
+loss_func = torch.nn.MSELoss()
+
+# set up optimizer
+optim = torch.optim.Adam(list(rnn.parameters()) + [W_r], lr=lr, betas=betas)
+rnn.clip(gradient_cutoff)
+
+# training
+train_loss = 0.0
+loss_col = []
+with torch.enable_grad():
+    for batch in range(batches):
+
+        loss = torch.zeros((1,), device=device, dtype=dtype)
+
+        for trial in np.random.choice(train_trials, size=(batch_size,), replace=False):
+
+            # get input and target timeseries
+            inp = torch.tensor(inputs[trial], device=device, dtype=dtype)
+            inp += noise_lvl * torch.randn(*inp.shape, device=device)
+            target = torch.tensor(targets[trial], device=device, dtype=dtype)
+
+            # get initial state
+            for step in range(init_steps):
+                x = torch.randn(n_in, dtype=dtype, device=device)
+                rnn.forward(x)
+            rnn.detach()
+
+            # collect loss
+            y_col = []
+            for step in range(steps):
+                z = rnn.forward(inp[step])
+                y = W_r @ z
+                if step % truncation_steps == truncation_steps - 1:
+                    rnn.detach()
+                y_col.append(y)
+
+            # calculate loss
+            y_col = torch.stack(y_col, dim=0)
+            loss += loss_func(y_col, target)
+
+        # store and print loss
+        train_loss = loss.item()
+        loss_col.append(train_loss)
+        print(f"Training epoch {batch + 1} / {batches}: MSE = {loss_col[-1]}")
+        if train_loss < epsilon:
+            break
+
+        # make update
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        rnn.detach()
+
+# generate predictions
+test_loss, predictions, dynamics, test_conditions = [], [], [], []
+with torch.no_grad():
+    for trial in range(train_trials, trials):
+
+        # get input and target timeseries
+        inp = torch.tensor(inputs[trial], device=device, dtype=dtype)
+        target = torch.tensor(targets[trial], device=device, dtype=dtype)
+
+        # get initial state
+        for step in range(init_steps):
+            x = torch.randn(n_in, dtype=dtype, device=device)
+            rnn.forward(x)
+        rnn.detach()
+
+        # make prediction
+        y_col, z_col = [], []
+        for step in range(steps):
+            if step > auto_steps:
+                inp[step, :n_out] = y
+            z = rnn.forward(inp[step])
+            y = W_r @ z
+            y_col.append(y)
+            z_col.append(z)
+        predictions.append(np.asarray([y.detach().cpu().numpy() for y in y_col]))
+        dynamics.append(np.asarray([z.detach().cpu().numpy() for z in z_col]))
+
+        # calculate loss
+        loss = loss_func(torch.stack(y_col, dim=0), target)
+        test_loss.append(loss.item())
+        test_conditions.append(conditions[trial])
+
+print(f"Finished after {batch + 1} training epochs. Final loss: {loss_col[-1]}.")
+
+# save results
+results = {"train_loss": loss_col, "test_loss": test_loss, "predictions": predictions,
+           "targets": targets[train_trials:], "conditions": test_conditions}
+pickle.dump(results, open(save_file, "wb"))
+
+if visualize_results:
+
+    # prediction figure
+    fig, axes = plt.subplots(nrows=plot_examples, figsize=(12, 2 * plot_examples))
+    for i, trial in enumerate(np.random.choice(test_trials, size=(plot_examples,))):
+        ax = axes[i]
+        ax.plot(targets[train_trials + trial], label="targets", linestyle="dashed")
+        for j, line in enumerate(ax.get_lines()):
+            ax.plot(predictions[trial][:, j], label="predictions", linestyle="solid",
+                    color=line.get_color())
+        ax.set_ylabel("amplitude")
+        ax.set_title(f"test trial {trial + 1}")
+        if i == plot_examples - 1:
+            ax.set_xlabel("steps")
+            ax.legend()
+    fig.suptitle("Model Predictions")
+    plt.tight_layout()
+
+    # dynamics figure
+    fig, axes = plt.subplots(nrows=plot_examples, figsize=(12, 2 * plot_examples))
+    n_neurons = 5
+    for i in range(plot_examples):
+        mean_v = np.mean(dynamics[i], axis=-1)
+        ax = axes[i]
+        ax.plot(mean_v, color="black", label="mean")
+        for j in np.random.choice(k, size=(n_neurons,)):
+            ax.plot(dynamics[i][:, j], label=f"neuron {j + 1}")
+        ax.axvline(x=int(0.5 * steps), color="grey", linestyle="dashed")
+        ax.set_ylabel("amplitude")
+        if i == plot_examples - 1:
+            ax.set_xlabel("steps")
+            ax.legend()
+    fig.suptitle("RNN dynamics")
+    plt.tight_layout()
+
+    # training loss figure
+    fig, ax = plt.subplots(figsize=(12, 4))
+    ax.plot(loss_col)
+    ax.set_xlabel("training batch")
+    ax.set_ylabel("MSE")
+    ax.set_title("Training loss")
+    plt.tight_layout()
+
+    plt.show()
--- scripts/clr_rhythmic_plotting.py
+++ scripts/clr_rhythmic_plotting.py
@@ -12,8 +12,9 @@ conditions = ["fit", "cfit"]
 # matplotlib settings
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rc('text', usetex=True)
-plt.rcParams['figure.dpi'] = 100
-plt.rcParams['figure.figsize'] = (12, 4)
+plt.rcParams['figure.constrained_layout.use'] = True
+plt.rcParams['figure.dpi'] = 200
+plt.rcParams['figure.figsize'] = (12.0, 7.0)
 plt.rcParams['font.size'] = 10.0
 plt.rcParams['axes.titlesize'] = 12
 plt.rcParams['axes.labelsize'] = 12
@@ -21,8 +22,14 @@ plt.rcParams['lines.linewidth'] = 1.0
 markersize = 6
 
 # collect data
+##############
+
 results = {"condition": [], "sigma": [], "Delta": [], "trial": [], "train_epochs": [], "train_loss": [], "test_loss": []}
+examples = {}
+
 for cond in conditions:
+
+    # sweep data
     data = pickle.load(open(f"{path}/{task}_{cond}.pkl", "rb"))
     for key, val in data.items():
         if type(val[0]) is list:
@@ -30,37 +37,80 @@ for cond in conditions:
         if key in results:
             results[key].extend(val)
     results["condition"].extend([cond]*len(data["trial"]))
+
+    # example fits
+    examples[cond] = pickle.load(open(f"{path}/{task}_{cond}_single.pkl", "rb"))
+
 df = DataFrame.from_dict(results)
 
-# find
-deltas = np.unique(df.loc[:, "Delta"].values)
+# plotting
+##########
 
 # create figure
 fig = plt.figure()
-grid = fig.add_gridspec(nrows=len(deltas), ncols=2)
-for i, Delta in enumerate(deltas):
+grid = fig.add_gridspec(nrows=5, ncols=4)
 
-    # constrain data to particular Delta
-    df_tmp = df.loc[df.loc[:, "Delta"] == Delta, :]
+# plot number of training epochs
+ax = fig.add_subplot(grid[:2, 0])
+sb.lineplot(df, x="sigma", y="train_epochs", hue="condition")
+ax.set_title("Number of training epochs")
+ax.set_ylabel("epochs")
+ax.set_xlabel(r"$\sigma$")
 
-    # plot number of training epochs
-    ax = fig.add_subplot(grid[i, 0])
-    sb.lineplot(df_tmp, x="sigma", y="train_epochs", hue="condition")
-    ax.set_title("Number of training epochs")
-    ax.set_ylabel("epochs")
-    ax.set_xlabel(r"$\sigma$")
+# plot examples of training progress
+ax = fig.add_subplot(grid[:2, 1])
+for cond in conditions:
+    ax.plot(examples[cond]["train_loss"], label=cond)
+ax.set_xlabel("training epoch")
+ax.set_ylabel("MSE")
+ax.set_title(r"Training progress for $\sigma = 0.6$")
+ax.legend()
 
-    # plot test loss
-    ax = fig.add_subplot(grid[i, 1])
-    sb.lineplot(df_tmp, x="sigma", y="test_loss", hue="condition")
-    ax.set_title("Loss on test trials")
-    ax.set_ylabel("MSE")
-    ax.set_xlabel(r"$\sigma$")
+# plot combined test loss
+ax = fig.add_subplot(grid[:2, 2])
+sb.lineplot(df, x="sigma", y="test_loss", hue="condition")
+ax.set_title("Average prediction performance")
+ax.set_ylabel("MSE")
+ax.set_xlabel(r"$\sigma$")
+
+# plot example test loss for specific conditions
+ax = fig.add_subplot(grid[:2, 3])
+cond_results = {"RNN": [], "tau": [], "MSE": []}
+for cond in conditions:
+    test_conds, test_loss = examples[cond]["conditions"], examples[cond]["test_loss"]
+    cond_indices = []
+    for c, loss in zip(test_conds, test_loss):
+        cond_results["RNN"].append(cond)
+        cond_results["tau"].append(c[1])
+        cond_results["MSE"].append(loss)
+df_tmp = DataFrame.from_dict(cond_results)
+sb.barplot(df_tmp, x="tau", y="MSE", hue="RNN")
+ax.set_title(r"Prediction performance for $\sigma = 0.6$")
+ax.set_xlabel(r"$\tau$")
+
+# plot example time series for each condition
+unique_conditions = np.unique(examples[conditions[0]]["conditions"], axis=0).tolist()
+cond_colors = ["royalblue", "darkorange"]
+for j, c1 in enumerate(unique_conditions):
+    ax = fig.add_subplot(grid[2+j, :])
+    for i, c2 in enumerate(conditions):
+        data = examples[c2]
+        test_conds, targs, preds = data["conditions"], data["targets"], data["predictions"]
+        idx = test_conds.index(tuple(c1))
+        if i == 0:
+            ax.plot(targs[idx][:, 0], color="black", label="target")
+        ax.plot(preds[idx][:, 0], color=cond_colors[i], label=c2)
+    ax.axvline(x=100, linestyle="dashed", color="grey")
+    ax.set_ylabel("y")
+    ax.set_title(rf"Predictions for $\tau = {c1[1]}$")
+    if j == 2:
+        ax.set_xlabel("steps")
+        ax.legend()
 
 # padding
 fig.set_constrained_layout_pads(w_pad=0.03, h_pad=0.01, hspace=0., wspace=0.)
 
 # saving/plotting
 fig.canvas.draw()
-plt.savefig(f'/home/richard-gast/Documents/results/clr_fitting.svg')
+plt.savefig(f'/home/richard-gast/Documents/results/clr_rhythmic_fitting.svg')
 plt.show()
