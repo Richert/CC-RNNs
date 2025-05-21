ac9fce18406d586e856065062434e6d240fe69d8
Richert <richgast.90@gmail.com>
2025-05-21 22:30:21 +0200

    updates on rhythmic learning task

--- scripts/clr_bifurcations_plotting.py
+++ scripts/clr_bifurcations_plotting.py
@@ -23,7 +23,7 @@ markersize = 6
 # collect data
 ##############
 
-results = {"lambda": [], "trial": [], "repetition": [], "train_epochs": [], "train_loss": [], "test_loss": [], "mu": [],
+results = {"lambda": [], "trial": [], "repetition": [], "train_epochs": [], "srl_loss": [], "test_loss": [], "mu": [],
            "c_dim": []}
 conceptors = []
 data = pickle.load(open(f"{path}/{task}_zfit.pkl", "rb"))
@@ -34,7 +34,7 @@ for trial in range(len(data["test_loss"])):
     results["lambda"].append(data["lambda"][trial])
     results["repetition"].append(data["repetition"][trial])
     results["train_epochs"].append(data["train_epochs"][trial])
-    results["train_loss"].append(data["train_loss"])
+    results["srl_loss"].append(data["srl_loss"])
     results["test_loss"].append(data["test_loss"][trial])
     results["mu"].append(data["mu"][trial])
     results["c_dim"].append(data["c_dim"][trial])
@@ -56,7 +56,7 @@ ax.set_title("Number of training epochs")
 ax.set_ylabel("epochs")
 ax.set_xlabel(r"$\lambda$")
 
-# plot combined test loss
+# plot test loss
 ax = fig.add_subplot(grid[0, 1])
 sb.barplot(df, x="lambda", y="test_loss", hue="condition")
 ax.set_title("Average prediction performance")
@@ -80,8 +80,15 @@ ax.set_title("Conceptor dimensionalities")
 ax.set_xlabel(r"$\lambda$")
 ax.set_ylabel("dim(C)")
 
+# plot conceptor loss
+ax = fig.add_subplot(grid[1, 0])
+sb.barplot(df, x="lambda", y="srl_loss", hue="condition")
+ax.set_title("Conceptor loss")
+ax.set_xlabel(r"$\lambda$")
+ax.set_ylabel("loss(C)")
+
 # plot example conceptors for each condition
-unique_lambdas = np.unique(df.loc[:, "lambda"].values)[[0, 2, 4]]
+unique_lambdas = np.unique(df.loc[:, "lambda"].values)[[1, 3]]
 for i, lam in enumerate(unique_lambdas):
     idx1 = df.loc[:, "lambda"] == lam
     cs = []
@@ -89,7 +96,7 @@ for i, lam in enumerate(unique_lambdas):
         idx2 = df.loc[:, "condition"] == cond
         idx = np.argmin(df.loc[idx1 & idx2, "test_loss"].values)
         cs.append(conceptors[idx1 & idx2][idx])
-    ax = fig.add_subplot(grid[1, 1+i])
+    ax = fig.add_subplot(grid[1, 2+i])
     ax.imshow(np.asarray(cs), aspect="auto", interpolation="none", cmap="cividis")
     ax.set_ylabel("condition")
     ax.set_xlabel("soma")
